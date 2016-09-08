13a8d2ac9a1f5f8b29a9a4d2df28d2656069ee01
Jessica Bohn <bohnjessica15@gmail.com>
2016-09-08 17:04:41 -0500

    Change contacts tab to user profile tab
    There was a tab in the bottom nav bar that linked to a contacts page
    but we have no need or want for that in our app, so it was changed
    to be a tab for the current user's profile page. The tab goes to
    the profile page of the logged in user where they can see all of their
    information. (possibly edit later)

--- BottomTabBar.js
+++ BottomTabBar.js
@@ -1,12 +1,19 @@
 'use strict';
 import React, { Component } from 'react';
-import { AppRegistry, TabBarIOS, View, Text } from 'react-native';
+import { AppRegistry,
+        TabBarIOS,
+        View,
+        Text,
+        AsyncStorage } from 'react-native';
+
 var Featured = require('./Featured');
 var Search = require('./Search');
 var Contacts = require('./Contacts');
 var Register = require('./Register');
 var Chat = require('./Chat');
 var UserProfile = require('./UserProfile')
+var Messages = require('./Messages')
+var Conversations = require('./Conversations')
 
 class BottomTabBar extends Component {
 
@@ -42,23 +49,25 @@ class BottomTabBar extends Component {
                     <Search/>
                 </TabBarIOS.Item>
                 <TabBarIOS.Item
-                systemIcon="contacts"
+                title="User"
+                    icon={{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAABXElEQVRIS7XVzyvlYRTH8Zf1sDAixnI2pERN2Gim/AFTs7CRsqYoJWpiwc4CRbGbmpTFJPYsLCyUH1kosZCFZmMzbCws6Kl767p97zPfe7/X2T7nfN7nOZ/nfL813jlq3llfWkArZvANH3GLnzj4X4NpAC04Q3OC2CF+4L4UKA1gEVORTlcxngUQxvA1AjjFlyyAI/RGAOfozgLYxfcIYCfnQ2JKGg/mMRsBTCP4VDFgHwMRwB8MZgH8wkgEsIzJLIA6XCHsQ3FcowtPWQChdgUTCSILmIttcxqTQ30DLtFYIPYX7XisBiBoXKCjQOwYPTHxcJb2BiHvDp8KBG/wuRqAeiyVeEkbCHtQckyxG3RiFEOojXT6gN9Yz/n0JjUJ0JRLDp/hcuIFWxjDv3xhMeADTtBWjnJRbvh39OE5yeRAX8sgni8dxmYSIMy7vwqAPWyX80wrZqbdg4oBr8vSMhm3xe9KAAAAAElFTkSuQmCC'}}
                     selected={this.state.selectedTab === 'userprofile'}
                     onPress={() => {
                         this.setState({
-                            selectedTab: 'userprofile'
+                            selectedTab: 'userprofile',
+                            notifCount: this.state.notifcount + 1,
                         });
                     }}>
                     <UserProfile/>
                 </TabBarIOS.Item>
                 <TabBarIOS.Item
-                title="Chat"
+                title="Messages"
                     badge={this.state.notifCount > 0 ? this.state.notifCount : undefined}
                     icon={{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAABzElEQVRIS7XWO2gVQRTG8V9AK8VGRG0UhCRibCxipYUKPtJIII0WMZ2FYm+INqI2IuIDRCWN2giKSdQENARtkspGfBZKCgtBwU4RXxyYCZvLRtd714Et9uzO9585O+c72+Y/j7Z/1F+EX/hRdd7fAJ3owfp0rcZizOIVXuM+3iwE/BPgJNoxg5dJ8F0SWleAbsFzHCuDlAE2YhyDuF4xFQdwArvSYuamNQK24RD6Kgo3vnYH5/AkPygCluMRNjUpnqc9w1Z8jkAR8Bj78b5FwFoMY0cR0ItIz5EWxfP0S5jAWN7BKbzAjZoAA4iTdjwDHuJwOtd1MDbgLHYHYCmuYl8dygWNW+jPO5j35WsAReWfx84MiB2cqTFF/ejAUAbMBWpYfUhcSDU1kgFhYFGFe/GzRUgY4k1sbyy0lbiH7hYBTyP3+NgIiPtVGMXmJiG3cRFTZV6UYwG5lgzva0VQHPHTqXdEwS7opvlB2HW46tsUiMKJXvAl3a9JpyQaUhhbNKCjZd+vrB90pSO7J4ldxgpEnwjAEnxIXSw62gNEHZWOMsAVTOMTDiKMK0RiLMM3VE3dPLsOgegJd/Edk8lCYrVNj7IdRCz+HGoZvwHTNVQh3MJKqQAAAABJRU5ErkJggg=='}}
-                    selected={this.state.selectedTab === 'Chat'}
+                    selected={this.state.selectedTab === 'Conversations'}
                     onPress={() => {
                         this.setState({
-                            selectedTab: 'Chat',
+                            selectedTab: 'Conversations',
                             notifCount: this.state.notifcount + 1,
                         });
                     }}>
